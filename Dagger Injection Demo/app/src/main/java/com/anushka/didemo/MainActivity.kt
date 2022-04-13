cbe322558a152aa50cb8c712ab8e5ab0384150e6
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-04-13 17:34:06 -0300

    Basic injection with Dagger 2

--- Dagger Injection Demo/app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ Dagger Injection Demo/app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        DaggerSmartPhoneComponent.create()
+            .getSmartPhone()
+            .makeACallWithRecording()
 
-        val smartPhone = SmartPhone(
+       /* val smartPhone = SmartPhone(
             Battery(),
             SIMCard(ServiceProvider()),
             MemoryCard()
         )
-            .makeACallWithRecording()
-
-
+            .makeACallWithRecording()*/
     }
 }
--- Dagger Injection Demo/app/src/main/java/com/anushka/didemo/SmartPhone.kt
+++ Dagger Injection Demo/app/src/main/java/com/anushka/didemo/SmartPhone.kt
@@ -1,8 +1,10 @@
 package com.anushka.didemo
 
 import android.util.Log
+import javax.inject.Inject
 
-class SmartPhone(val battery: Battery, val simCard: SIMCard, val memoryCard: MemoryCard) {
+//es necesario hacer el constructor publico para que el dagger pueda inyectar el objeto
+class SmartPhone @Inject constructor(val battery: Battery, val simCard: SIMCard, val memoryCard: MemoryCard) {
 
     init {
         battery.getPower()
