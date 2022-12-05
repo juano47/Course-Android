12311ad1854b85f3defa7b191e2881c3b27e6bb1
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-12-05 17:38:19 +0100

    Basic WorkManager functionality

--- WorkManagerDemo1/app/src/main/java/com/delaiglesia/workmanagerdemo1/MainActivity.kt
+++ WorkManagerDemo1/app/src/main/java/com/delaiglesia/workmanagerdemo1/MainActivity.kt
@@ -2,10 +2,25 @@ package com.delaiglesia.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.delaiglesia.workmanagerdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
     }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploaderWorker::class.java).build()
+        WorkManager.getInstance(this).enqueue(uploadRequest)
+    }
+
 }
---
+++ WorkManagerDemo1/app/src/main/java/com/delaiglesia/workmanagerdemo1/UploaderWorker.kt
@@ -0,0 +1,22 @@
+package com.delaiglesia.workmanagerdemo1
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploaderWorker(context: Context, params:WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        try {
+            for (i in 0..600) {
+                Log.d("MYTAG", "Uploading $i")
+            }
+            return Result.success()
+        } catch (e: Exception) {
+            Log.e("MYTAG", "Error uploading", e)
+            return Result.failure()
+        }
+
+    }
+
+}
