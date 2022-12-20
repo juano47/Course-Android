77ec1dbd6611e2fb68c7e19708e343c7ef07cfe1
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-12-20 11:35:57 +0100

    News App: add api service (retrofit interface)

---
+++ NewsApp/app/src/main/java/com/delaiglesia/newsapp/data/api/NewsApiService.kt
@@ -0,0 +1,17 @@
+package com.delaiglesia.newsapp.data.api
+
+import com.delaiglesia.newsapp.BuildConfig
+import com.delaiglesia.newsapp.data.model.APIResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApiService {
+
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country") country: String = "us",
+        @Query("page") page: Int = 1,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
+    ): Response<APIResponse>
+}
