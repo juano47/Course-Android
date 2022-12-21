8083e2099fdb4517a5ae30a8610b5896e8beec5b
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-12-21 01:44:59 +0100

    News App: Fragments and Navigation components

--- NewsApp/app/src/main/java/com/delaiglesia/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/delaiglesia/newsapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.delaiglesia.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.delaiglesia.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        val navController = navHostFragment?.findNavController()
+        binding.bnvNews.setupWithNavController(navController!!)
     }
 }
