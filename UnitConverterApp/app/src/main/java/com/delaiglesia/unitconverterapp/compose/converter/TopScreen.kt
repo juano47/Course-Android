5c43ba5dfad510a92637d94f4dd9c4b755449fc1
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2023-01-05 17:47:22 +0100

    Unit Converter App: learn about side-effects

--- UnitConverterApp/app/src/main/java/com/delaiglesia/unitconverterapp/compose/converter/TopScreen.kt
+++ UnitConverterApp/app/src/main/java/com/delaiglesia/unitconverterapp/compose/converter/TopScreen.kt
@@ -1,13 +1,16 @@
 package com.delaiglesia.unitconverterapp.compose.converter
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.delaiglesia.unitconverterapp.data.Conversion
 import java.text.DecimalFormat
 
@@ -20,6 +23,16 @@ fun TopScreen(
     isLandScape: Boolean,
     saveResult: (String, String) -> Unit
 ) {
+    //cada vez que se produce una recomposicion, se va a mostrar el toast, esto se conoce como side effect!
+    //Toast.makeText(LocalContext.current, "A test message that should only be displayed once", Toast.LENGTH_SHORT).show()
+    //Como evitarlo? Usar side effects API's.
+    val context = LocalContext.current
+    LaunchedEffect(
+        key1 = true, //true es un valor que no cambia, por lo que solo se ejecutara una vez
+        block = {
+        Toast.makeText(context, "A test message that should only be displayed once", Toast.LENGTH_SHORT).show()
+    })
+
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         ConversionMenu(
             list = list,
